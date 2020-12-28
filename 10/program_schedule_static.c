addbff2bdbba7a88c48fc85f999869ec06d71400
liuchaorun <chaorunliu@gmail.com>
2020-12-28 15:41:21 +0800

    update

---
+++ 10/program_schedule_static.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <omp.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+int main(int argc, char* argv[]) {
+    int thread_count = strtol(argv[1], NULL, 10);
+    int n = strtol(argv[2], NULL, 10);
+
+#pragma omp parallel for num_threads(thread_count) schedule(static)
+    for (int i = 0; i < n; i ++) {
+        printf("i=%d, thread_id=%d\n", i, omp_get_thread_num());
+    }
+
+    return 0;
+}
